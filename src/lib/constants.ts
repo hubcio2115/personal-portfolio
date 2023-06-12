d3d60957b134f597ef9c5d0f2e070b03e02317cc
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-06-12 18:36:06 +0200

    added some constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const COMMANDS = [
+  'whois',
+  'video',
+  'projects',
+  'social',
+  'secret',
+  'clear',
+] as const;
+
+export const PROMPT = 'hubertkowalski@macbook-pro ~ %';
+
+export const CARET_SYMBOL = '█';
