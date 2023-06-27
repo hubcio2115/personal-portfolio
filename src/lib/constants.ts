97602df04f71646dac29ba92b34d9afcde791472
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-06-27 17:43:58 +0200

    added messages for each command

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -14,3 +14,41 @@ export const ASCII_ART = ` _   _       _               _     _  __              
 |_| |_|\\__,_|_.__/ \\___|_|   \\__| |_|\\_\\___/ \\_/\\_/ \\__,_|_|___/_|\\_\'_| ©2023`;
 
 export const CARET_SYMBOL = '█';
+
+export const WHOIS_MESSAGE = `Hey, I'm Hubert! 👋🏻
+I'm a software developer and uni CS student, who likes to build engaging websites like this one
+After graduating I'd like to continue my strive in frontend development and I'm looking forward to
+see how it will turn out.
+By this point I got opportunities to work at some software houses via multiple internship programs.
+I also created a lot of projects relating different areas of computer science, such is the way of a
+university student.
+I'm working on exploring new things about web dev by trying new tools, learning about new things and
+trying to implement them. Like this site. It uses Astro, a web framework for hosting static web pages.
+It introduces ideas that I was not thought at uni, like islands architecture, the idea of MPA and many more!
+Now I'm looking for more opportunities to spread my wings and learn from veterans in the field. Also I'm on the
+last stretch of my Bachelor's degree.
+
+I hope you'll find this little web page I made on a whim fun to interact with!`;
+
+export const WHOAMI_MESSAGE = `Who are you? I don't know, why would you think I know?`;
+
+export const PROJECTS_MESSAGE = `Here is a little list of my projects:
+  - <a href="https://pokemon-smash-pass.vercel.app/" target="_blank">🫐 pokemon-smash-pass</a>
+  - <a href="https://snake-eosin-one.vercel.app/" target="_blank">🐍 snake</a>
+  - <a href="https://github.com/hubcio2115/personal-dbms-nextjs" target="_blank">💽 personal-dbms-nextjs</a>
+  - <a href="https://github.com/hubcio2115/movies-app-frontend" target="_blank">🍿 movies-app</a>`;
+
+export const SOCIAL_MESSAGE = `linkedin   <a href="https://www.linkedin.com/in/hubert-kowalski-447aaa213/" target="_blank">linkedin/hubertkowalski</a>
+github     <a href="https://www.github.com/hubcio2115\" target="_blank">github/hubcio2115</a>`;
+
+export const HELP_MESSAGE = `whois - Who am I?
+whoami - Who are you?
+projects - View my projects
+social - View my social media accounts
+clear - Clears the terminal
+help - You already know what it does
+github - Get the link to this site's project on github`;
+
+export const GITHUB_MESSAGE = `<a href="https://github.com/hubcio2115/personal-portfolio/" target="_blank">link</a>`;
+
+export const COMMAND_NOT_FOUND_MESSAGE = 'zsh: command not found:';
