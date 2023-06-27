d0b2cbc1f80e123c2a970370067b20d974fe8072
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-06-27 18:05:59 +0200

    added gruvbox inspired styles for the page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,15 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        'dark-body': '#282828',
+        'dark-text': '#ebdbb2',
+        'dark-caret': '#fe8019',
+
+        'light-body': '#fbf1c7',
+        'light-text': '#3c3836',
+        'light-caret': '#d65d0e',
+      },
       fontFamily: {
         sans: ['JetBrainsMono', ...defaultTheme.fontFamily.sans],
       },
