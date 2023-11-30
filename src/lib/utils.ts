ade0f375546d8276a82139c9c844b7f786fecd51
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-11-30 17:35:59 +0100

    added parseBlogPosts utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,16 @@
+import type { CollectionEntry } from "astro:content";
+
 export function clearTextArea(input: HTMLTextAreaElement) {
 	input.setSelectionRange(0, 0);
 	input.focus();
 	input.value = "";
+}
+
+export function parseBlogPosts(posts: CollectionEntry<"blog">[]) {
+	return posts
+		.map(
+			({ id, data }) =>
+				`.rw-r--r-- hubertkowalski staff ${data.publishDate.toLocaleDateString()} <a href="/blog/${id}">${id}</a>`,
+		)
+		.join("\n");
 }
