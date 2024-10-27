d0d70930b2ebdab2a5b4d6bd76c9cff415e6c174
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-10-27 19:13:37 +0100

    updated utils for modifying theme

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,23 @@
 import type { CollectionEntry } from "astro:content";
 
-export function clearTextArea(input: HTMLTextAreaElement) {
-	input.setSelectionRange(0, 0);
-	input.focus();
-	input.value = "";
+export function getTheme() {
+	if (typeof localStorage !== "undefined" && localStorage.getItem("theme"))
+		return localStorage.getItem("theme");
+
+	if (window.matchMedia("(prefers-color-scheme: dark)").matches) return "dark";
+
+	return "light";
 }
 
-export function toggleTheme(theme: "light" | "dark"): "light" | "dark" {
+export function setTheme(theme: "light" | "dark") {
 	if (theme === "dark") {
-		document.documentElement.classList.remove("dark");
-		localStorage.setItem("theme", "light");
-		return "light";
-	} else {
 		document.documentElement.classList.add("dark");
+		document.documentElement.classList.remove("light");
 		localStorage.setItem("theme", "dark");
-		return "dark";
+	} else {
+		document.documentElement.classList.remove("dark");
+		document.documentElement.classList.add("light");
+		localStorage.setItem("theme", "light");
 	}
 }
 
