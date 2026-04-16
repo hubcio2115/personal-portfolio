6278dbbed7bb51931b1bd5c9cdcb0d23874034da
hubcio2115 <kowalski.hubercik1@gmail.com>
2026-04-16 09:53:27 +0200

    upgraded tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
-import tailwind from "@astrojs/tailwind";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind({
-			config: { applyBaseStyles: false },
-		}),
-	],
+	vite: {
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				"@components": fileURLToPath(
+					new URL("./src/components", import.meta.url),
+				),
+				"@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+				"~": fileURLToPath(new URL("./src", import.meta.url)),
+			},
+		},
+	},
 	site: "https://personal-portfolio-one-tan.vercel.app/",
 });
