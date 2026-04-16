ebf048d98c36ef0d9d264f63c3a3b3f99ab391b9
hubcio2115 <kowalski.hubercik1@gmail.com>
2026-04-16 10:01:12 +0200

    updated eslint config to include ts strict

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,22 @@
 import astro from "eslint-plugin-astro";
+import tseslint from "@typescript-eslint/eslint-plugin";
 
-export default [...astro.configs["flat/recommended"]];
+export default [
+  {
+    ignores: [".astro/**", "dist/**"],
+  },
+  ...tseslint.configs["flat/strict"],
+  ...astro.configs["flat/recommended"],
+  {
+    files: ["**/*.astro"],
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
+  {
+    files: ["**/*.d.ts"],
+    rules: {
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
+];
